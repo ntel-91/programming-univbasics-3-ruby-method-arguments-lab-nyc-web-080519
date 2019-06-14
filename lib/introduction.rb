30ec74e653ac468d410f81348d0141bd3c2d54e3
Nick Telenson <ntelenson@gmail.com>
2019-06-14 19:16:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,3 +10,8 @@ def introduction_with_language_optional (name)
 
   puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
+
+def introduction_with_language_optional (name, language)
+
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
